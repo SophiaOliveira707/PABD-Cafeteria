783f83509f9002bb07a2382c12ecc614571be9a1
Sophia Oliveira <sophialaviniaoliveira@gmail.com>
2024-10-20 16:15:38 -0300

    imagens renomeadas

--- janelas/tela_login.py
+++
@@ -1,8 +0,0 @@
-from funcoes.sql import *
-
-def login(bancoDeDados:Bd_cafe,usuario,senha):
-    bancoDeDados.executar(f'SELECT * FROM usuarios WHERE usuario = {usuario} AND senha = {senha}')
-    usuarios = bancoDeDados.pegarValores()
-    if usuarios != None:
-        #faz login (muda a tela)
-        pass
